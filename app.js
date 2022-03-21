d7631f54f5151fe8249a5d1428c7f72619c5630c
fx64b <fabiomaffucci.outlook.com@zbw-online.ch>
2022-03-21 21:25:08 +0100

    Changed: Dots Animation to Terminal Typewriter Animation

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-var loading = document.getElementById("loading");
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// improve animation or make it entirely in css
-//
-async function load() {
-    while(true) {
-        loading.innerHTML = "&nbsp;";
-        await sleep(1000);
-        loading.innerText = ".";
-        await sleep(1000);
-        loading.innerText = "..";
-        await sleep(1000);
-        loading.innerText = "...";
-        await sleep(1000);
+async function loadingAnimation()  {
+    var loading = document.getElementById("loading");
+    var user = document.getElementById("user");
+    // wait for a short time
+    await sleep(1000);
+
+    // blinking effect
+    for(var i = 0; i<3; i++) {
+        user.innerHTML = '[fabio@maffucci ~]$ <span class="cursor">_</span>';
+        await sleep(600);
+        user.innerHTML = "[fabio@maffucci ~]$ ";
+        await sleep(600);
+    }
+
+    // typewriter effect
+    var string = "start website";
+ 
+for(var i = 0; i<string.length; i++) {
+    if(i<string.length) {
+        loading.innerHTML += string.charAt(i);
+        await sleep(Math.random() * 320);
     }
 }
 
-load();
+}
+
+loadingAnimation();
